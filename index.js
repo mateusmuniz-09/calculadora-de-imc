a04eff24557c7e51c48fd46d1eacee6385ed69bf
Mateus Muniz <mateus09muniz@gmail.com>
2025-04-03 15:50:29 -0300

    alteração no html, e correção de escritas

--- index.js
+++ index.js
@@ -8,75 +8,52 @@ const btnLimpar = document.getElementById("btn-limpar");
 
 btnCalcular.addEventListener("click", function () {
   const peso = parseFloat(valor.value);
-  if (isNaN(peso) || peso === "") {
-    alert("Peso inválido, digite apenas numeros!");
+  if (isNaN(peso) || valor.value.trim() === "") {
+    alert("Peso inválido! Digite apenas números.");
     valor.value = "";
     return;
   }
 
   const altura = parseFloat(valorAltura.value);
-  if (isNaN(altura) || altura === "") {
-    alert("Altura inválida, digite apenas numeros!");
+  if (isNaN(altura) || valorAltura.value.trim() === "") {
+    alert("Altura inválida! Digite apenas números.");
     valorAltura.value = "";
     return;
   }
+
   let alturaConvertida = altura / 100;
-  let imc = parseFloat(peso / (alturaConvertida * alturaConvertida));
+  let imc = peso / (alturaConvertida * alturaConvertida);
+
   if (imc < 18.5) {
     resultadoFinal = "Você está abaixo do peso.";
-    resultado.textContent = resultadoFinal;
-    spanImc.textContent = imc.toFixed(1);
-    spanImc.style.color = "#333";
     spanImc.style.backgroundColor = "#FFD700";
-    resultado.style.color = "#333";
     resultado.style.backgroundColor = "#FFD700";
-    return;
-  } else if (imc === 18.5 || imc <= 24.9) {
+  } else if (imc >= 18.5 && imc <= 24.9) {
     resultadoFinal = "Seu peso está normal.";
-    resultado.textContent = resultadoFinal;
-    spanImc.textContent = imc.toFixed(1);
-    spanImc.style.color = "#fff";
     spanImc.style.backgroundColor = "#32CD32";
-    resultado.style.color = "#fff";
     resultado.style.backgroundColor = "#32CD32";
-    return;
-  } else if (imc === 25.0 || imc <= 29.9) {
-    resultadoFinal = "⚠️Atenção, você está com sobrepeso";
-    resultado.textContent = resultadoFinal;
-    spanImc.textContent = imc.toFixed(1);
-    spanImc.style.color = "#fff";
+  } else if (imc >= 25.0 && imc <= 29.9) {
+    resultadoFinal = "⚠️ Atenção, você está com sobrepeso.";
     spanImc.style.backgroundColor = "#FFA500";
-    resultado.style.color = "#fff";
     resultado.style.backgroundColor = "#FFA500";
-    return;
-  } else if (imc === 30.0 || imc <= 34.9) {
-    resultadoFinal = "⚠️Atenção, obesidade grau I";
-    resultado.textContent = resultadoFinal;
-    spanImc.textContent = imc.toFixed(1);
-    spanImc.style.color = "#fff";
+  } else if (imc >= 30.0 && imc <= 34.9) {
+    resultadoFinal = "⚠️ Atenção, obesidade grau I.";
     spanImc.style.backgroundColor = "#FF4500";
-    resultado.style.color = "#fff";
     resultado.style.backgroundColor = "#FF4500";
-    return;
-  } else if (imc === 35.0 || imc <= 39.9) {
-    resultadoFinal = "⚠️Atenção, obesidade grau II";
-    resultado.textContent = resultadoFinal;
-    spanImc.textContent = imc.toFixed(1);
-    spanImc.style.color = "#fff";
-    spanImc.style.backgroundColor = "##FF4500";
-    resultado.style.color = "#fff";
-    resultado.style.backgroundColor = "##FF4500";
-    return;
-  } else if (imc > 40) {
-    resultadoFinal = "⚠️Atenção, obesidade grau III (Mórbida)";
-    resultado.textContent = resultadoFinal;
-    spanImc.textContent = imc.toFixed(1);
-    spanImc.style.color = "#fff";
+  } else if (imc >= 35.0 && imc <= 39.9) {
+    resultadoFinal = "⚠️ Atenção, obesidade grau II.";
+    spanImc.style.backgroundColor = "#FF4500";
+    resultado.style.backgroundColor = "#FF4500";
+  } else if (imc >= 40) {
+    resultadoFinal = "⚠️ Atenção, obesidade grau III (Mórbida).";
     spanImc.style.backgroundColor = "#8B0000";
-    resultado.style.color = "#fff";
     resultado.style.backgroundColor = "#8B0000";
-    return;
   }
+
+  resultado.textContent = resultadoFinal;
+  spanImc.textContent = imc.toFixed(1);
+  spanImc.style.color = "#fff";
+  resultado.style.color = "#fff";
 });
 
 btnLimpar.addEventListener("click", () => {
